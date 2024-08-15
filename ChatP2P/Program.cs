c2eacd4baa0da168ccdef2aa685cd435d503b1d8
Alonso Gutierrez Varela <agutierrez109@alumnos.uaq.mx>
2024-08-15 16:01:13 -0600

    feat: api de chat p2p

---
+++ ChatP2P/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ChatP2P;
+
+public class Program{
+    public static async Task Main(string[] args){
+        var peer = new Peer();
+        if (args.Length > 0 && args[0] == "connect" 
+            && !string.IsNullOrEmpty(args[1]) 
+            && !string.IsNullOrEmpty(args[2]))        {
+
+            // Convertir args[2] a int
+            if (int.TryParse(args[2], out int port)){
+                await peer.ConnectToPeer(args[1], port);
+            } else {
+                Console.WriteLine("El segundo argumento debe ser un número válido (puerto).");
+            }
+        }
+        else
+        {
+            await peer.StartListening();
+        }
+    }
+}
